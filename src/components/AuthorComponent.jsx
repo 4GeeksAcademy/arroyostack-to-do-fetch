d9e2938f6cd5d468296c3139ec43e656ee9c38c5
arroyostack <arroyostack@gmail.com>
2023-07-18 20:45:21 +0200

    Author component created

---
+++ src/components/AuthorComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AuthorComponent = () => {
+    return (
+        <div class="card container author-title text-center bg-dark">
+            <div >
+                <p className='pt-3 text-white'>Made with <span><i class="bi bi-peace-fill"></i></span> and <span><i class=" text-danger bi bi-heart-fill"></i></span> <a href="https://github.com/arroyostack" target="blank">arroyostack</a></p>
+            </div>
+        </div>
+    );
+};
